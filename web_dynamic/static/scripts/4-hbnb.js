af880d654c6b63bd6693685d9de09a60491f1635
MMhenna <mhenna.mehdi@gmail.com>
2021-09-27 21:27:59 +0100

    commit

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -15,11 +15,41 @@ $('document').ready(function () {
     data: '{}',
     contentType: 'application/json',
     dataType: 'json',
-    success: function (data) {
-      $('SECTION.places').append(data.map(place => {
-        return `<ARTICLE>
-                  <DIV class="title">
-                    <H2>${place.name}</H2>
+    success: appendPlaces
+  });
+
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
+  });
+
+  $('BUTTON').click(function () {
+    $.ajax({
+      url: api + ':5001/api/v1/places_search/',
+      type: 'POST',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: appendPlaces
+    });
+  });
+});
+
+function appendPlaces (data) {
+  $('SECTION.places').empty();
+  $('SECTION.places').append(data.map(place => {
+    return `<ARTICLE>
+                <DIV class="title">
+                  <H2>${place.name}</H2>
                     <DIV class="price_by_night">
                       ${place.price_by_night}
                     </DIV>
@@ -45,21 +75,5 @@ $('document').ready(function () {
                     ${place.description}
                   </DIV>
                 </ARTICLE>`;
-      }));
-    }
-  });
-
-  const amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenities[$(this).attr('data-id')];
-    }
-    if (Object.values(amenities).length === 0) {
-      $('.amenities H4').html('&nbsp;');
-    } else {
-      $('.amenities H4').text(Object.values(amenities).join(', '));
-    }
-  });
-});
+  }));
+}
